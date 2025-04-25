5bae1ceea399dbfb36d53dd548ba105441ce8c04
Jina <trannnhii2407@gmail.com>
2025-04-25 17:15:54 +0800

    Add option to convert Celsius to Fahrenheit

--- weather.py
+++ weather.py
@@ -19,10 +19,25 @@ class City:
         self.temp_max = response_json["main"]["temp_max"]
         self.temp_min = response_json["main"]["temp_min"]
     
-    def print_data(self):
+    def print_data_celsius(self):
         print(f"In {self.city}, today is {self.temp}°C")
         print(f"Highest temperature is {self.temp_max}°C")
-        print(f"Lowest temperature is {self.temp_min}°C")
+        print(f"Lowest temperature is {self.temp_min}°C\n")
+    
+    def print_data_fahrenheit(self):
+        self.change_unit()
+        print(f"In {self.city}, today is {self.temp_fahrenheitUnit:.2f}°F")
+        print(f"Highest temperature is {self.temp_max_fahrenheitUnit:.2f}F")
+        print(f"Lowest temperature is {self.temp_min_fahrenheitUnit:.2f}°F\n")
+
+    def change_unit(self):
+        self.temp_fahrenheitUnit = (self.temp * 9/5) + 32
+        self.temp_max_fahrenheitUnit = (self.temp_max * 9/5) + 32
+        self.temp_min_fahrenheitUnit = (self.temp_min * 9/5) + 32
 
 myCity = City("Perth", -31.954800499999997, 115.85837364402198)
-myCity.print_data()
+myCity.print_data_celsius()
+myCity.print_data_fahrenheit()
+
+
+    
